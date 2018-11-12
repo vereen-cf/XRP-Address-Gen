7367da5f16b1c3724a2b74a3116573e835174afe
vereen-cf <44467638+vereen-cf@users.noreply.github.com>
2018-11-12 15:48:14 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,5 +3,4 @@ function newripple() {
   var account = api.generateAddress();
   document.getElementById("address").value = account.address;
   document.getElementById("secret").value = account.secret;
-  console.log("Ripple Address: " + account.address + "\nRipple Secret: " + account.secret);
 }
